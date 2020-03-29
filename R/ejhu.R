7f708f8b598b8dbceeb725dcae9de85a3af1d086
Vince Carey <stvjc@channing.harvard.edu>
2020-03-29 12:35:24 -0400

    added nytstate data hander

--- R/ejhu.R
+++ R/ejhu.R
@@ -1,10 +1,37 @@
+#' create cumulative_events instance for state data from NYT
+#' @param src a tibble
+#' @param eventtype character(1) 'confirmed' or 'deaths'
+#' @param statename character(1) state name
+#' @examples
+#' nyt = nytimes_state_data()
+#' mass = cumulative_events_nyt_state(nyt, eventtype = "confirmed",
+#'    statename = "Massachusetts")
+#' mass
+#' imass = form_incident_events(trim_from(mass, "2020-03-01"))
+#' plot(imass)
+#' @export
+cumulative_events_nyt_state = function (src, eventtype = "confirmed", 
+      statename = "Massachusetts") {
+    stopifnot(statename %in% src$state)
+    cur = src %>% dplyr::filter(subset == eventtype & state == statename)
+    cumul = cur$count
+    dates = cur$date
+    ans = list(count = cumul, dates = dates)
+    attr(ans, "ProvinceState") = statename
+    attr(ans, "source") = "NYT"
+    attr(ans, "dtype") = "cumulative"
+    class(ans) = c("cumulative_events", "covid_events")
+    ans
+}
+
 #' print for a covid_events instance
 #' @param x covid_events instance
 #' @param \dots not used
 #' @export
 print.covid_events = function(x, ...)  {
+cursrc = attr(x, "source")
 cat(sprintf("%s event data for %s, %s to %s", attr(x, "dtype"),
-  attr(x, "alpha3"), min(x$dates), max(x$dates)), "\n")
+  attr(x, ifelse(cursrc == "JHU", "alpha3", "ProvinceState")), min(x$dates), max(x$dates)), "\n")
 cat("use plot() to visualize.\n")
 }
 
@@ -19,6 +46,8 @@ trim_from = function(x, date) UseMethod("trim_from")
 #' @param date string representing date in yyyy-mm-dd form
 #' @export
 trim_from.covid_events = function(x, date = "2020-02-15") {
+  chk = substr(date, 1, 4)
+  if (chk != "2020") warning("expecting year 2020 in trim date, please check")
   st = lubridate::as_date(date)
   ok = which(x$dates >= st)
   x$dates = x$dates[ok]
@@ -34,8 +63,9 @@ trim_from.covid_events = function(x, date = "2020-02-15") {
 #' @param \dots not used
 #' @export
 plot.covid_events = function (x, main=NULL, ylab=NULL, xlab=NULL,  ...) {
+  sr = attr(x, "source")
   if (is.null(main)) main = paste0(attr(x, "dtype"), 
-            " events for ", attr(x, "alpha3"))
+            " events for ", attr(x, ifelse(sr=="JHU", "alpha3", "ProvinceState")))
   if (is.null(ylab)) ylab = paste0(attr(x, "dtype"))
   if (is.null(xlab)) xlab = "Date"
   plot(x$dates, x$count, main=main, ylab=ylab, xlab=xlab, ...)
@@ -57,6 +87,7 @@ cumulative_events_ejhu = function(src, eventtype = "confirmed",
  dates = cur$date
  ans = list(count=cumul, dates=dates)
  attr(ans, "alpha3") = alpha3
+ attr(ans, "source") = "JHU"
  attr(ans, "ProvinceState") = ProvinceState
  attr(ans, "dtype") = "cumulative"
  class(ans) = c("cumulative_events", "covid_events")
